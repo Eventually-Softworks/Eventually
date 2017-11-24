54fa73a3e87e3886ba30ec8e975b2759acff3d91
JTorrus <jtorrusgalindo@gmail.com>
2017-11-24 18:17:16 +0100

    Layouts and resource files edited. Colors added to buttons.

--- app/src/main/java/com/example/javier_t/eventuallytest/Objects/User.kt
+++ app/src/main/java/com/example/javier_t/eventuallytest/Objects/User.kt
@@ -1,9 +1,5 @@
 package com.example.javier_t.eventuallytest.Objects
 
-/**
- * Created by Javier_T on 11/5/2017.
- */
-
 class User () {
     lateinit var email: String
     lateinit var password: String
