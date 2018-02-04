f99e99dfb7bc3f21beaef901e3c44ca035bae82f
JTorrus <jtorrusgalindo@gmail.com>
2018-02-04 18:28:12 +0100

    Action bar with navigation drawer implemented, TODO functionality

--- app/src/main/java/com/evesoftworks/javier_t/eventually/activities/MainPageActivity.kt
+++ app/src/main/java/com/evesoftworks/javier_t/eventually/activities/MainPageActivity.kt
@@ -5,7 +5,8 @@ import android.support.design.widget.TabLayout
 import android.support.v7.app.AppCompatActivity
 
 import android.os.Bundle
-import android.view.Menu
+import android.support.design.widget.NavigationView
+import android.support.v7.app.ActionBarDrawerToggle
 import android.view.MenuItem
 
 
@@ -15,11 +16,18 @@ import com.evesoftworks.javier_t.eventually.fragments.ContactsFragment
 import com.evesoftworks.javier_t.eventually.fragments.EventsFragment
 import com.evesoftworks.javier_t.eventually.fragments.GroupsFragment
 import kotlinx.android.synthetic.main.activity_main_page.*
+import kotlinx.android.synthetic.main.app_bar.*
+import kotlinx.android.synthetic.main.tabs_layout.*
 
-class MainPageActivity : AppCompatActivity(), ContactsFragment.OnFragmentInteractionListener, EventsFragment.OnFragmentInteractionListener, GroupsFragment.OnFragmentInteractionListener {
+class MainPageActivity : AppCompatActivity(), ContactsFragment.OnFragmentInteractionListener, EventsFragment.OnFragmentInteractionListener, GroupsFragment.OnFragmentInteractionListener, NavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     override fun onFragmentInteraction(uri: Uri) {}
 
     private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
+    private lateinit var mToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,20 +37,19 @@ class MainPageActivity : AppCompatActivity(), ContactsFragment.OnFragmentInterac
         mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
         container.adapter = mSectionsPagerAdapter
 
+        mToggle = ActionBarDrawerToggle(this, main_content, R.string.sidebaropen, R.string.sidebarclosed)
+        main_content.addDrawerListener(mToggle)
+        mToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navigation_drawer.setNavigationItemSelectedListener(this)
+
         container.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
         tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(container))
     }
 
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main_page, menu)
-        return true
-    }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-
-        if (id == R.id.action_settings) {
+        if (mToggle.onOptionsItemSelected(item)) {
             return true
         }
 
