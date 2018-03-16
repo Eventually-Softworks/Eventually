a1a11ab0d16f7b19a749d19d8c4dde46c2d6be52
JTorrus <jtorrusgalindo@gmail.com>
2018-03-16 07:00:14 +0100

    Single Event activity and layout implemented with CollapsingToolbarLayout

--- app/src/main/java/com/evesoftworks/javier_t/eventually/activities/AnEventActivity.kt
+++ app/src/main/java/com/evesoftworks/javier_t/eventually/activities/AnEventActivity.kt
@@ -2,12 +2,38 @@ package com.evesoftworks.javier_t.eventually.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout
+import android.view.MenuItem
 import com.evesoftworks.javier_t.eventually.R
+import com.evesoftworks.javier_t.eventually.databaseobjects.Event
+import kotlinx.android.synthetic.main.activity_an_event.*
 
 class AnEventActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_an_event)
+        val bundle = intent.extras
+        val event = bundle.getParcelable<Event>("anEvent")
+
+        aneventtoolbar.title = event.name
+
+        setSupportActionBar(aneventtoolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        aneventname.text = event.name
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+
+        return true
     }
 }
