c9d948dc289d72e363df52b5d16f0662eef44513
JTorrus <jtorrusgalindo@gmail.com>
2018-04-14 17:22:19 +0200

    User can upload a profile pic to Firebase Storage, to do: ask for desired app to select an image

--- app/src/main/java/com/evesoftworks/javier_t/eventually/activities/DataCompletionActivity.kt
+++ app/src/main/java/com/evesoftworks/javier_t/eventually/activities/DataCompletionActivity.kt
@@ -8,10 +8,14 @@ import android.os.Binder
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.support.design.widget.Snackbar
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.TabHost
 import com.evesoftworks.javier_t.eventually.R
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_data_completion.*
 import java.io.IOException
@@ -22,7 +26,12 @@ class DataCompletionActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(view: View?) {
         when (view?.id) {
             R.id.data_completion_profile_pic -> {
+                chooseImage()
+            }
 
+            R.id.fab_to_grid -> {
+                uploadImage()
+//                goToGridSelectionActivity()
             }
         }
     }
@@ -30,6 +39,9 @@ class DataCompletionActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data_completion)
+
+        data_completion_profile_pic.setOnClickListener(this)
+        fab_to_grid.setOnClickListener(this)
     }
 
     private fun chooseImage() {
@@ -57,7 +69,20 @@ class DataCompletionActivity : AppCompatActivity(), View.OnClickListener {
     private fun uploadImage() {
         filePath.let {
             val progressBar = ProgressBar(this)
-            val storageReference: StorageReference
+            progressBar.max = 100
+            val storageReference: StorageReference = FirebaseStorage.getInstance().reference.child("usersprofilepics/${FirebaseAuth.getInstance().currentUser!!.uid}")
+
+            storageReference.putFile(filePath).addOnSuccessListener {
+                progressBar.progress = 100
+            }.addOnFailureListener({
+                Snackbar.make(findViewById(R.id.data_completion), "Ha ocurrido un error al subir la imagen", Snackbar.LENGTH_LONG)
+            })
         }
+    }
+
+    private fun goToGridSelectionActivity() {
+        val intent = Intent(this, GridSelectionActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
