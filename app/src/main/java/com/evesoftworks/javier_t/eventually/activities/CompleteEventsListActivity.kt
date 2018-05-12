1f85507339f1fa1096d725595c6616efe7acf3ff
JTorrus <jtorrusgalindo@gmail.com>
2018-05-12 06:59:42 +0200

    Retrieving each section from EventsFragment to CompleteEventsListActivity correctly, to do: query implementation to get events

--- app/src/main/java/com/evesoftworks/javier_t/eventually/activities/CompleteEventsListActivity.kt
+++ app/src/main/java/com/evesoftworks/javier_t/eventually/activities/CompleteEventsListActivity.kt
@@ -2,6 +2,7 @@ package com.evesoftworks.javier_t.eventually.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.evesoftworks.javier_t.eventually.R
 
 class CompleteEventsListActivity : AppCompatActivity() {
@@ -9,5 +10,9 @@ class CompleteEventsListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_complete_events_list)
+
+        if (intent.extras?.getString("aCompleteEvent") != null) {
+            Toast.makeText(this, intent.extras?.getString("aCompleteEvent"), Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/com/evesoftworks/javier_t/eventually/adapters/EventSectionAdapter.kt
+++ app/src/main/java/com/evesoftworks/javier_t/eventually/adapters/EventSectionAdapter.kt
@@ -1,11 +1,14 @@
 package com.evesoftworks.javier_t.eventually.adapters
 
+import android.content.Intent
+import android.os.Bundle
 import android.support.v7.widget.CardView
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.evesoftworks.javier_t.eventually.R
+import com.evesoftworks.javier_t.eventually.activities.CompleteEventsListActivity
 import com.evesoftworks.javier_t.eventually.dbmodel.EventSection
 import kotlinx.android.synthetic.main.single_event_section.view.*
 
@@ -30,6 +33,13 @@ class EventSectionAdapter(val sectionArray: ArrayList<EventSection>): RecyclerVi
         holder.cardView.eventRecycle.setHasFixedSize(true)
         holder.cardView.eventRecycle.layoutManager = LinearLayoutManager(holder.cardView.context, LinearLayoutManager.HORIZONTAL, false)
 
-     //   sectionArray[position].
+        holder.cardView.show_more_button.setOnClickListener {
+            val intent = Intent(holder.cardView.context, CompleteEventsListActivity::class.java)
+            val bundle = Bundle()
+            bundle.putString("aCompleteEvent", sectionArray[position].title)
+            intent.putExtras(bundle)
+
+            holder.cardView.context.startActivity(intent)
+        }
     }
 }
